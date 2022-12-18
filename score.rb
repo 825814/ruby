be304dcf91d75a071bca97644dd1f3ef0ce4c106
825814 <+825814@users.noreply.github.com>
2022-12-18 15:11:12 +0000

    [finish]ruby-5

---
+++ score.rb
@@ -0,0 +1,8 @@
+score = 70
+if (score >= 50 || score <=100) && score >=80
+  puts "得点は５０点以上１００点以下で、かつ８０点以上です。"
+end
+
+if score >= 50 || (score <= 100 && score >= 80)
+  puts "得点は５０点以上、または８０点以上１００点以下です。"
+end
