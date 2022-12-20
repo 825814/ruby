c7a51b0c56ba104360179790068e64f858028226
825814 <+825814@users.noreply.github.com>
2022-12-20 13:58:29 +0000

    [finish]ruby-11

---
+++ override.rb
@@ -0,0 +1,18 @@
+class Car
+ def run(distance)
+   puts "車で#{distance}キロ走ります。"
+ end
+end
+
+class Bus < Car
+  def run(distance)
+    super
+    puts "30人を乗せて、走っています。"
+  end
+end
+
+bus = Bus.new
+bus.run(5)
+
+# car = Car.new
+# car.run(5)
