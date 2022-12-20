f8bb7d361beac43271b91358e58e54c10aab14ec
825814 <+825814@users.noreply.github.com>
2022-12-20 13:37:48 +0000

    [finish]ruby-10

---
+++ inheritance.rb
@@ -0,0 +1,18 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Bus<Car
+end
+
+#   def run(distance)
+#     puts "車で#{distance}キロ走ります。"
+#   end
+# end
+
+bus=Bus.new
+bus.run(5)
+
+puts Bus.superclass
---
+++ lesson10.rb
@@ -0,0 +1,11 @@
+class Car
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+class Taxi < Car
+end
+
+taxi = Taxi.new
+taxi.run(4)
