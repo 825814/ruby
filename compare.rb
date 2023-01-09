a9dfd13bbfbab5a4c07016394726606549bc700e
825814 <120297399+825814@users.noreply.github.com>
2023-01-09 13:15:13 +0000

    [add]ruby-5

--- compare.rb
+++ compare.rb
@@ -4,5 +4,15 @@ if total < 200
 end
 
 if total >= 150
-  puts '合計は１５０以上です'
+  puts '合計は150以上です'
+end
+
+
+total = 100
+if total < 200
+  puts "合計は200未満です"
+end
+
+if total >= 150
+  puts "合計は150以上です"
 end
--- hand.rb
+++ hand.rb
@@ -10,3 +10,30 @@ end
 if (hand == "グー") || (hand == "パー")
   puts "出した手はグーまたはパーです"
 end
+
+hand = "グー"
+if hand == "グー"
+  puts "出した手はグーです"
+end
+
+if hand != "チョキ"
+  puts "出した手はチョキではありません"
+end
+
+if (hand == "グー") || (hand == "パー")
+  puts "出した手はグーまたはパーです"
+end
+
+# hand = "グー"
+# if hand == "グー"
+#   puts "出した手はグーです"
+# end
+
+# if hand != "チョキ"
+#   puts "出した手はチョキではありません"
+# end
+
+# if (hand == "グー") || (hand == "パー")
+# 　puts "出した手はグーまたはパーです"
+# end
+
