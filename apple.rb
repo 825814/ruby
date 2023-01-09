04367550b20f80e9c687d58c63ed004b4f3cd674
825814 <120297399+825814@users.noreply.github.com>
2023-01-09 13:29:10 +0000

    [add]ruby-5

--- apple.rb
+++ apple.rb
@@ -6,6 +6,15 @@ elsif apple == "Nagano"
 else
   puts "このリンゴは青森県産でも長野県産でもありません。"
 end
+
+apple = "Yamanashi"
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
   
   
     
