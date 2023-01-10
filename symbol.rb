9042deeb7505bfc8deff42b1e6283bb5ecd9437d
825814 <120297399+825814@users.noreply.github.com>
2023-01-10 14:23:43 +0000

    [add]ruby-12

--- symbol.rb
+++ symbol.rb
@@ -1,3 +1,15 @@
 webcamp = :プログラミング学習
 puts webcamp
 
+webcamp = :プログラミング学習
+puts webcamp
+
+dog1 = "犬"
+dog2 = "犬"
+dog3 = :犬
+dog4 = :犬
+
+puts dog1.object_id
+puts dog2.object_id
+puts dog3.object_id
+puts dog4.object_id
