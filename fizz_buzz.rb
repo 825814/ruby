524fb9fa4c2c069fd2aa2d9f66ffcaf12871bcd5
825814 <120297399+825814@users.noreply.github.com>
2023-01-10 11:04:42 +0000

    [add]ruby-8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,6 +1,26 @@
+# def fizz_buzz(number)
+#   if number % 15 == 0
+#     "FizzBuzz"
+#   elsif number % 5 == 0
+#     "Buzz"
+#   elsif number % 3 == 0
+#     "Fizz"
+#   else
+#     number.to_s
+#   end
+# end
+
+# puts "数字を入力してください"
+
+# input = gets.to_i
+
+# puts "結果は..."
+# puts fizz_buzz(input)
+ 
+ 
 def fizz_buzz(number)
   if number % 15 == 0
-    "FizzBuzz"
+    "fizzBuzz"
   elsif number % 5 == 0
     "Buzz"
   elsif number % 3 == 0
@@ -15,10 +35,7 @@ puts "数字を入力してください"
 input = gets.to_i
 
 puts "結果は..."
-puts fizz_buzz(input)
- 
- 
-    
+puts fizz_buzz(input)    
         
       
       
--- greeting.rb
+++ greeting.rb
@@ -5,3 +5,10 @@ end
 
 puts greeting('John')
 
+def greeting(name)
+  return "Hello, #{name}!"
+  "Good morning, #{name}!"
+end
+
+puts greeting("John")
+
